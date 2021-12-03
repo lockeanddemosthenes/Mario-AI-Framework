c609b4f554d4faf1cc5b43c498ffbaf3dbb0e539
Errica <errica.cheng@gmail.com>
2021-12-02 20:03:12 -0500

    additional tweaks so our agent uses A*

--- src/agents/chengHuntNagy/Agent.java
+++ src/agents/chengHuntNagy/Agent.java
@@ -7,15 +7,18 @@ import engine.helper.MarioActions;
 
 public class Agent implements MarioAgent {
     private boolean[] action;
+    private PathFinder pathFinder;
 
     @Override
     public void initialize(MarioForwardModel model, MarioTimer timer) {
         this.action = new boolean[MarioActions.numberOfActions()];
+        this.pathFinder = new PathFinder();
     }
 
     @Override
     public boolean[] getActions(MarioForwardModel model, MarioTimer timer) {
-        action = new boolean[]{false, true, false, true, true};
+        action = this.pathFinder.optimise(model, timer);
+        // action = new boolean[]{false, true, false, true, true};
         return action;
     }
 
